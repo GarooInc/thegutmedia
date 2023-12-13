d2a0a2e7a9a04f658c9e2e7cb270bdfb466db8a5
Jimena Hernández <her21199@uvg.edu.gt>
2023-12-13 11:32:01 -0600

    Update Tittle.jsx

--- components/Tittle/Tittle.jsx
+++ components/Tittle/Tittle.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
-const Tittle = ( {title,center} ) => {
+
+const Tittle = ({ title, center }) => {
     return (
-    <h1 className="text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl font-inter text-center md:text-start " style={center ? {textAlign: 'center'} : {textAlign: 'left'}}>
-        {title}
-    </h1>
+        <h1 className={`text-4xl font-bold tracking-tight text-gray-900 sm:text-6xl font-inter text-center ${center ? 'md:text-center' : 'md:text-start'}`}>
+            {title}
+        </h1>
     )
 }
 
-export default Tittle
+export default Tittle;
