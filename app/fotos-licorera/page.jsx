729be0178b2dbee48524a7411f39582fef59b884
Jimena Hernández <her21199@uvg.edu.gt>
2024-07-25 14:53:37 -0600

    Update page.jsx

--- app/fotos-licorera/page.jsx
+++ app/fotos-licorera/page.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const FotosLicorera = () => {
   return (
-    <div className='mt-20 w-full h-screen'>
+    <div className='pt-20 w-full overflow-hidden h-screen flex justify-center items-center'>
         <iframe 
-        width="100%"
-        height="100%" 
+        width="90%"
+        height="90%" 
         src="https://lookerstudio.google.com/embed/reporting/16dc4e1b-f70a-4bdc-b7fb-1316f82fd39b/page/s9ccC" 
         frameBorder="0"
         style={{ border: '0' }}
