ab28d15012fbf51f768c8caad5e956c7b9dfd21d
Jimena Hernández <her21199@uvg.edu.gt>
2024-08-08 09:09:14 -0600

    update pages

---
+++ app/fotos-otis/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const FotosOtis = () => {
+  return (
+    <div className='pt-20 w-full overflow-hidden h-screen flex justify-center items-center'>
+        <iframe 
+        width="90%"
+        height="90%" 
+        src="https://lookerstudio.google.com/embed/reporting/83127ec3-8d6f-469a-b75d-e428abebffbd/page/s9ccC" 
+        frameBorder="0"
+        style={{ border: '0' }}
+        allowFullScreen
+        sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+        ></iframe>
+    </div>
+  )
+}
+
+export default FotosOtis
