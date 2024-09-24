3d0da092cd76897eb73947b8e4e3aa8dec983dd7
Jimena Hernández <her21199@uvg.edu.gt>
2024-09-24 14:50:09 -0600

    Create page.jsx

---
+++ app/credichapin/page.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const CrediChapin = () => {
+  return (
+    <div className='pt-20 w-full overflow-hidden h-screen flex justify-center items-center'>
+        <iframe 
+        width="90%"
+        height="90%" 
+        src="https://lookerstudio.google.com/embed/reporting/cdfddf1c-7719-4ddf-bb34-83750092b183/page/s9ccC" 
+        frameBorder="0"
+        style={{ border: '0' }}
+        allowFullScreen
+        sandbox="allow-storage-access-by-user-activation allow-scripts allow-same-origin allow-popups allow-popups-to-escape-sandbox"
+        ></iframe>
+    </div>
+  )
+}
+
+export default CrediChapin
